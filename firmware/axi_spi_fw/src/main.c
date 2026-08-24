512c7c90324c83a2bb6ba7f79606086fbf2b34f9
gyeongminbag27-arch <gyeongminbag27@gmail.com>
2026-08-24 21:53:55 +0900

    Add FPGA STM32 SPI communication test

--- firmware/axi_spi_fw/src/main.c
+++ firmware/axi_spi_fw/src/main.c
@@ -1,21 +1,40 @@
-#include "xil_printf.h"
+#include <stdint.h>
 
 #include "spi/spi.h"
-#include "device/device_gpio.h"
+#include "spi/spi_device.h"
 
 int main(void)
 {
-    xil_printf("\r\nAXI SPI FW Start\r\n");
-    device_gpio_init();
+    volatile uint8_t rx = 0;
 
-    /* SPI Mode 0, 우선 느린 clock */
+    /* GPIO 기반 CS 초기화 */
+    spi_device_init();
+
+    /* SPI Mode 0
+       clk_div = 10
+       CPOL = 0
+       CPHA = 0
+    */
     spi_init(10, 0, 0);
-    xil_printf("GPIO init complete\r\n");
 
-    xil_printf("SPI init complete\r\n");
+    /* STM32 선택 후 0xA5 전송
+       STM32가 보내는 1Byte를 rx에 저장 */
+    rx = spi_transaction(SPI_DEVICE_STM32, 0xA5);
 
-    while (1)
+    /* 여기에서 Breakpoint를 걸어 rx 값을 확인 */
+    if (rx == 0x3C)
     {
+        while (1)
+        {
+            /* PASS */
+        }
+    }
+    else
+    {
+        while (1)
+        {
+            /* FAIL */
+        }
     }
 
     return 0;
